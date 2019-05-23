3d72a36cfc6b6cd9ae92db7c8fe7ae9866609e62
ARTHUR HUTAGALUNG <45054498+vongalung@users.noreply.github.com>
2019-05-23 13:51:00 +0700

    Add files via upload
    - Adding tempo control
    - Adding capabilities to support custom notes

--- audio_generator.py
+++ audio_generator.py
@@ -6,24 +6,37 @@ class audio(object):
 		self._s_rate = None
 		self._sample = None
 		self._audio = np.array([])
-		self._blank = None
+		self._notes = notes.NOTES
+		self._notes_per_second = notes.NOTES_PER_SECOND
 	def set_sampling(self,rate=44100):
-		if not self._sample is None:
-			raise Exception("Sample Rate has been initialized as %d!" % self._sample)
+		if not self._s_rate is None:
+			raise Exception("Sample Rate has been initialized as %d!" % self._s_rate)
 		elif isinstance(rate,int):
 			self._s_rate = rate
-			self._sample = np.arange(0,1,1/self._s_rate)
-			self._blank = np.tile(0,self._s_rate)
 		else:
 			raise Exception("Sample Rate should be of integer type!")
+	def set_notes(self,note_set=None,note_second=None):
+		if note_set is None:
+			None
+		elif isinstance(note_set,list):
+			self._notes = note_set
+		else:
+			raise Exception("Notes should be in a list!")
+		if note_second is None:
+			None
+		elif isinstance(note_second,int):
+			self._notes_per_second = note_second
+		else:
+			raise Exception("Notes per Second should be of integer type!")
 
-	def __note(self,name,duration,funct):
-		return np.tile(funct(notes.NOTES[name],self._sample),duration)
+	def __note(self,name,funct):
+		return funct(self._notes[name],self._sample)
 	def generate(self,tones,duration=1,funct=SIN):
 		try:
-			aud=np.tile(self._blank,duration)
+			self._sample = np.arange(0,duration/self._notes_per_second,1/self._s_rate)
+			aud=np.tile(0,len(self._sample))
 			for tone in tones:
-				aud=aud+self.__note(tone,duration,funct)
+				aud=aud+self.__note(tone,funct)
 			return aud
 		except Exception as e:
 			raise e
@@ -39,5 +52,7 @@ class audio(object):
 	def normalize(self,audio_buffer):
 		norm = (audio_buffer*32767)/np.max(np.abs(audio_buffer))
 		return norm.astype(np.int16)
-	def run_audio(self,audio_buffer=None,s_rate=None):
+	def run_audio(self,audio_buffer=None,s_rate=None,reset=False):
 		basicplayer.play(self.normalize(self._audio if audio_buffer is None else audio_buffer),(self._s_rate if s_rate is None else s_rate))
+		if reset:
+			reset_audio()
